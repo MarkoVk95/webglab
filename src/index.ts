8daff71faa7fe8d631bf270dc652bd568a98d263
Marko V <marko.vk@outlook.com>
2019-11-17 01:15:53 +0200

    setup initial uniforms view and proj

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,13 @@ import boxVertexShader from './shaders/boxVertexShader.glsl'
 import boxFragmentShader from './shaders/boxFragmentShader.glsl'
 import Drawable from './js/drawable'
 import { glMatrix, mat4, mat3, vec3 } from 'gl-matrix'
-import {boxIndices,boxVertices, colorRGB} from './resources/boxVectors'
+import { boxIndices, boxVertices, colorRGB } from './resources/boxVectors'
 class CanvasApp {
     private canvas: HTMLCanvasElement;
     private gl: WebGLRenderingContext;
     private program: WebGLProgram;
-    private viewMatrix: Float32Array;
-    private projMatrix: Float32Array;
+    private viewMatrix: mat4;
+    private projMatrix: mat4;
     private objectList: Drawable[] = [];
     constructor() {
         this.canvas = document.getElementById('game-canvas') as HTMLCanvasElement;
@@ -23,25 +23,40 @@ class CanvasApp {
 
         this.setupProgram();
         this.setupBuffers();
+        this.setupInitialUniforms();
         //this.setupListeners();
         requestAnimationFrame(this.mainLoop.bind(this));
 
     }
 
-    private setupBuffers(): void {
-        const gl:WebGLRenderingContext = this.gl;
-        const program:WebGLProgram = this.program;
+    private setupInitialUniforms(): void {
+        this.gl.useProgram(this.program);
+        const matViewUniformLocation: WebGLUniformLocation = this.gl.getUniformLocation(this.program, 'mView');
+        const matProjUniformLocation: WebGLUniformLocation = this.gl.getUniformLocation(this.program, 'mProj');
+        //setup -VIEWMATRIX-
+        this.viewMatrix = mat4.create();
+        mat4.lookAt(this.viewMatrix, [150, -150, 550], [150.0, -150.0, 0.0], [0.0, 1.0, 0.0]);
+        //setup -PROJECTION MATRIX-
+        this.projMatrix = mat4.create();
+        mat4.perspective(this.projMatrix, glMatrix.toRadian(45),this.gl.canvas.width  / this.gl.canvas.height, 1, 2000);
+        //setup -INITIAL UNIFORMS-
+        this.gl.uniformMatrix4fv(matViewUniformLocation, false, this.viewMatrix);
+        this.gl.uniformMatrix4fv(matProjUniformLocation, false, this.projMatrix);
+    }
 
-        const boxVertexBufferObject:WebGLBuffer = gl.createBuffer();
+    private setupBuffers(): void {
+        const gl: WebGLRenderingContext = this.gl;
+        const program: WebGLProgram = this.program;
+
+        const boxVertexBufferObject: WebGLBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, boxVertexBufferObject);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(boxVertices), gl.STATIC_DRAW);
-        console.log(boxVertices);
-        const boxIndexBufferObject:WebGLBuffer = gl.createBuffer();
+        const boxIndexBufferObject: WebGLBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, boxIndexBufferObject);
         gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(boxIndices), gl.STATIC_DRAW);
 
-        const positionAttribLocation:number = gl.getAttribLocation(program, 'vertPosition');
-        const colorAttribLocation:number = gl.getAttribLocation(program, 'vertColor');
+        const positionAttribLocation: number = gl.getAttribLocation(program, 'vertPosition');
+        const colorAttribLocation: number = gl.getAttribLocation(program, 'vertColor');
         gl.vertexAttribPointer(
             positionAttribLocation, // Attribute location
             3, // Number of elements per attribute
@@ -50,7 +65,7 @@ class CanvasApp {
             3 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
             0 // Offset from the beginning of a single vertex to this attribute
         );
-        var boxRGBBufferObject:WebGLBuffer = gl.createBuffer();
+        var boxRGBBufferObject: WebGLBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, boxRGBBufferObject);
         gl.bufferData(gl.ARRAY_BUFFER, new Uint8Array(colorRGB), gl.STATIC_DRAW);
 
@@ -70,7 +85,7 @@ class CanvasApp {
     private setupProgram(): void {
         const vertexShader: WebGLShader = this.compileShader(this.gl.VERTEX_SHADER, boxVertexShader);
         const fragmentShader: WebGLShader = this.compileShader(this.gl.FRAGMENT_SHADER, boxFragmentShader);
-        this.program =  this.createProgram(vertexShader, fragmentShader);
+        this.program = this.createProgram(vertexShader, fragmentShader);
     }
 
     private compileShader(SHADER_TYPE: number, shaderString: string) {
