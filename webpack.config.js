a131e244af0a2e6460c3cdecd2d857137ed6cd0f
Marko V <marko.vk@outlook.com>
2019-11-16 19:02:09 +0200

    .glsl webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,34 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: "development",
-    devtool: "source-map",
-    resolve: {
-      // Add `.ts` and `.tsx` as a resolvable extension.
-      extensions: [".ts", ".tsx", ".js"]
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: 'awesome-typescript-loader',
-                exclude: /node_modules/
-              },
-          {
-            test: /\.js$/,
-            use: ["source-map-loader"],
-            enforce: "pre"
-          }
-        ],
+  mode: "development",
+  devtool: "source-map",
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader',
+        exclude: /node_modules/
       },
-      plugins: [
-        new HtmlWebPackPlugin({
-          template: "./src/index.html",
-          filename: "./index.html"
-        })
-      ]
-  };
+      {
+        test: /\.glsl$/,
+        loader: 'webpack-glsl'
+      },
+      {
+        test: /\.js$/,
+        use: ["source-map-loader"],
+        enforce: "pre"
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
