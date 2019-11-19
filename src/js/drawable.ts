5c9f4aa14421cb121f052d135cb3e2cb32e7fa15
Marko V <marko.vk@outlook.com>
2019-11-19 17:30:44 +0200

    add initializeTexture()

--- src/js/drawable.ts
+++ src/js/drawable.ts
@@ -1,7 +1,9 @@
 export interface Drawable {
     draw(): void;
+    initializeTexture(): Promise<void>;
 }
 export abstract class WallContainer implements Drawable {
     abstract draw(): void;
+    abstract initializeTexture(): Promise<void>;
     abstract setWall(i:number, j:number):void;
 }
